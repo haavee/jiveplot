7f16f372952a43b1cc7d168c8cf0a13f1886c846
haavee <hverkouter@gmail.com>
2021-03-12 11:30:46 +0100

    Add comparison postprocessing step
    For comparing two datasets for equal data content the
        postprocessing/compare_data.compare_data()
    
    function can be used. If loaded, it always stores a copy of the latest plot that is generated after loading.
    
    Each time a plot is made, and its plot type is the same as the currently stored data set, all data sets between the new and stored data are compared; all differences between x- and y- arrays for data sets common to both plots are accumulated for both x and y.
    
    If any of the accumulated arrays (x and/or y) contain a difference > set tolerance (currently 1e-7), the compare_data() will raise a RuntimeError.
    
    If the new and previous plots do not have exactly the same data sets or have differing plot types, they compare unequal w/o even trying and a RuntimeError is raised.
    
    After the comparison but before raising a runtime error the new data set is installed as the new "last" dataset.
    
    Suggested sequence for testing:
    
        # install this module
        > postprocess postprocessing/compare_data.compare_data
    
        # Define which information and subset to extract that is expected to be numerically equivalent
        # between the two data sets. Make sure the 'pl' command is last - it triggers extraction of
        # the data and generates the plot datasets
        > macro extract "pt rnichan; fq ...; bl ...; indexr; scan nnn; pl"
    
        # Open reference data set and extract the relevant info
        > ms reference.ms;
        > extract
    
        # Now open data set that is expected to have
        # the exact same numerical content, by
        > ms test.ms
        > extract
    
    Profit!

---
+++ postprocessing/compare_data.py
@@ -0,0 +1,78 @@
+from   __future__ import print_function
+import numpy, plots, math, operator, copy
+
+COPY = copy.deepcopy
+# define a post-processing operation that always remembers the last data set and 
+# computes statistical differences between that one and the next data set.
+# After that, the new data set becomes the last one.
+
+# Purpose: to verify that two chunks of data extraced from measurement sets that *should*
+# be equal, are, in fact, equal.
+
+# If a new type of data set is detected (new plot type) the last data set is erased,
+# this data set becomes the stored one, and no output is generated
+
+last_dataset = None
+tolerance    = 1e-7
+
+def plotar2unidict(plotar):
+    rv = plots.Dict()
+    rv.plotType = plotar.plotType
+    # loop over all plots and datasets-within-plot
+    for k in plotar.keys():
+        for d in plotar[k].keys():
+            # get the full data set label - we have access to all the data set's properties (FQ, SB, POL etc)
+            n     = plots.join_label(k, d)
+            # and make a copy of the dataset
+            rv[n] = COPY( plotar[k][d] )
+    return rv
+
+# this is the function to pass to `postprocess ...`
+def compare_data(plotar, ms2mappings):
+    global last_dataset
+    # Whatever we need to do - this can be done unconditionally
+    new_dataset = plotar2unidict( plotar )
+    error       = ""
+    # Check if we need to do anything at all
+    if last_dataset is not None and last_dataset.plotType == new_dataset.plotType:
+        # OK check all common keys
+        old_keys = set(last_dataset.keys())
+        new_keys = set(new_dataset.keys())
+        # if the sets are not equal, the data will also not compare equal!
+        if old_keys == new_keys:
+            # inspect x and y separately, add up all the diffs
+            dx, dy = 0, 0
+            for k in old_keys:
+                ods = last_dataset[ k ]
+                nds = new_dataset[ k ]
+                dx  = numpy.add(numpy.abs( ods.xval - nds.xval ), dx)
+                dy  = numpy.add(numpy.abs( ods.yval - nds.yval ), dy)
+            print("dx=", dx)
+            print("dy=", dy)
+            if numpy.any( dx>abs(tolerance) ):
+                print(">>> compare_data: total diffs in x exceed tolerance")
+                print("    tolerance=", tolerance)
+                print("    accum. dx=", dx)
+                error += "Datasets mismatch in X according to tolerance. "
+            if numpy.any( dy>abs(tolerance) ):
+                print(">>> compare_data: total diffs in y exceed tolerance")
+                print("    tolerance=", tolerance)
+                print("    accum. dy=", dy)
+                error = "Datasets mismatch in Y according to tolerance. "
+        else:
+            print(">>> compare_data: The datasets to compare have different data content?!")
+            common = old_keys & new_keys
+            only_o = old_keys - common
+            only_n = new_keys - common
+            print("    Common keys:", len(common))
+            print("    Uniq in Old:", len(only_o))
+            print("    Uniq in New:", len(only_n))
+            error += "Datasets mismatch in content. "
+            #with open('/tmp/oldkeys.txt', 'w') as f:
+            #    list( map(lambda s: f.write(str(s) + '\n'), sorted(only_o)) )
+            #with open('/tmp/newkeys.txt', 'w') as f:
+            #    list( map(lambda s: f.write(str(s) + '\n'), sorted(only_n)) )
+    # Install new dataset as new last dataset
+    last_dataset = new_dataset
+    if error:
+        raise RuntimeError(error)
