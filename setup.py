109dff02cb138becdc0d17636109fa4dd3377a47
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2021-05-18 13:39:17 +0000

    UPdate setup file

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ pkg = 'jiveplot'
 __version__ = "1.0.0"
 build_root = os.path.dirname(__file__)
 
+requirements = [
+        'python-casacore',
+        'ppgplot @ git+git://github.com/haavee/ppgplot.git#egg=ppgplot',
+        #ppgplot'
+        ]
+
+#      dependency_links=['ppgplot @ git+git://github.com/haavee/ppgplot.git#egg=ppgplot'],
 def readme():
     """Get readme content for package long description"""
-    with open(os.path.join(build_root, 'README.rst')) as f:
+    with open(os.path.join(build_root, 'README.md')) as f:
         return f.read()
-
-
-def requirements():
-    """Get package requirements"""
-    with open(os.path.join(build_root, 'requirements.txt')) as f:
-        return [pname.strip() for pname in f.readlines()]
-
 
 setup(name=pkg,
       version=__version__,
@@ -32,7 +32,7 @@ setup(name=pkg,
                    "Topic :: Software Development :: Libraries :: Python Modules"],
       keywords="ms dataset statistics models plots",
       platforms=["OS Independent"],
-      install_requires=requirements(),
+      install_requires=requirements,
       python_requires='>=3.6',
       include_package_data=True,
-      scripts=['jiveplot/bin/jplotter'])
+      scripts=['jiveplot/bin/' + i for i in os.listdir('jiveplot/bin')])
