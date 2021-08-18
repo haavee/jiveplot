23984cb42e764f0e3dc3f47a7223f4afe0fc7781
haavee <hverkouter@gmail.com>
2021-08-18 16:31:12 +0200

    leftover Py2 -> Py2/3 compabitility issues
    Some code wasn't excecuted in the Py2 -> Py2/3 compatibility effort and
    executing them revealed some leftover bits

--- postprocessing/phatime.py
+++ postprocessing/phatime.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy, plots, math, operator
 
 def do_wrap(seq, low=-numpy.pi, high=numpy.pi):
@@ -27,14 +28,14 @@ def unwrap(plotar, ms2mappings):
 def drawline_fn(n, xv, yv):
     def f(device, **kwargs):
         xoffset = kwargs.get('xoffset', 0.0)
-        #print "    f(): drawing extra Y for {0}".format(n)
-        #print "    ",xv, yv
+        #print("    f(): drawing extra Y for {0}".format(n))
+        #print("    ",xv, yv)
         device.pgline(xv - xoffset, yv)
     return f
 
 def phaserate(plotar, ms2mappings):
     if plotar.plotType!='phatime':
-        raise RuntimeError, "phaserate() cannot run on plot type {0}".format( plotar.plotType )
+        raise RuntimeError( "phaserate() cannot run on plot type {0}".format(plotar.plotType) )
     spm = ms2mappings.spectralMap
     # iterate over all plots and all data sets within the plots
     for k in plotar.keys():
@@ -59,7 +60,7 @@ def phaserate(plotar, ms2mappings):
             f      = spm.frequencyOfFREQ_SB(n.FQ, n.SB)
             rate   = coeffs[0]
             if var_r<std_r:
-                print "{0}: {1:.8f} ps/s @ {2:5.4f}MHz [R2={3:.3f}]".format(n, rate/(2.0*numpy.pi*f*1.0e-12), f/1.0e6, r_sq )
+                print("{0}: {1:.8f} ps/s @ {2:5.4f}MHz [R2={3:.3f}]".format(n, rate/(2.0*numpy.pi*f*1.0e-12), f/1.0e6, r_sq ))
                 # before plotting wrap back to -pi,pi and transform to degrees
                 dsref.extra = [ drawline_fn(n, dsref.xval, numpy.rad2deg(do_wrap(extray))) ]
 
@@ -67,7 +68,7 @@ cache = {}
 def phasedbg(plotar, ms2mappings):
     global cache
     if plotar.plotType!='phatime':
-        raise RuntimeError, "phasedbg() cannot run on plot type {0}".format( plotar.plotType )
+        raise RuntimeError( "phasedbg() cannot run on plot type {0}".format(plotar.plotType) )
     store = len(cache)==0
     # iterate over all plots and all data sets within the plots
     for k in plotar.keys():
@@ -97,10 +98,10 @@ def phasedbg(plotar, ms2mappings):
                     # do differencing
                     otherCoeffs = cache.get(n, None)
                     if otherCoeffs is None:
-                        print "{0}: not found in cache".format( n )
+                        print("{0}: not found in cache".format(n))
                     else:
                         delta = otherCoeffs - coeffs
-                        print "{0.BL} {0.SB} {0.P}: dRate={1:5.4f} dOff={2:4.1f}".format(n, delta[0], delta[1]+360.0 if delta[1]<0.0 else delta[1])
+                        print("{0.BL} {0.SB} {0.P}: dRate={1:5.4f} dOff={2:4.1f}".format(n, delta[0], delta[1]+360.0 if delta[1]<0.0 else delta[1]))
                 # before plotting wrap back to -pi,pi and transform to degrees
                 #dsref.extra = [ drawline_fn(n, dsref.xval, numpy.rad2deg(do_wrap(extray))) ]
     if not store:
